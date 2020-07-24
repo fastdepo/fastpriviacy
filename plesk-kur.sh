9ecd47e792d427a9b082d09bcbe73c10fc1e3a3e
fastdepo <68726741+fastdepo@users.noreply.github.com>
2020-07-24 11:30:05 +0300

    Add files via upload

--- plesk-kur.sh
+++ plesk-kur.sh
@@ -3,8 +3,11 @@
 plesk_email='admin@test.tst'
 plesk_pass='PleskUbuntu123'
 plesk_name='admin'
+
 plesk_ui=spv
+
 fail2ban=yes
+
 http2=yes
 
 pci_compliance=false
@@ -12,9 +15,8 @@ pci_compliance=false
 clone=off
 
 if [ "$(id -u)" != "0" ]; then
-    echo "Error: You must be root to run this script, please use the root user to install the software."
     echo ""
-    echo "Use 'su - root' to login as root"
+    echo "Root olmadan çalışmaz. sudo su yazarak root ol."
     exit 1
 fi
 
@@ -31,6 +33,10 @@ readonly plesk_distro_version=$(lsb_release -sc)
 readonly plesk_distro_id=$(lsb_release -rs)
 readonly plesk_srv_arch="$(uname -m)"
 readonly plesk_kvm_detec=$(systemd-detect-virt)
+
+echo ""
+echo "Kuruluma hoşgeldin..."
+echo ""
 
 while [ "$#" -gt 0 ]; do
     case "$1" in
@@ -89,7 +95,7 @@ if [ "$interactive_install" = "y" ]; then
         done
         if [[ "$mariadb_server_install" == "y" ]]; then
             while [[ $mariadb_version_install != "10.1" && $mariadb_version_install != "10.2" && $mariadb_version_install != "10.3" ]]; do
-                echo -e "Versiyon [10.1 / 10.2 / 10.3] kurulsun. Örnek 10.3 : "
+                echo -e "Hangi versiyon kurulsun ? [10.1 / 10.2 / 10.3]: "
                 read -r mariadb_version_install
             done
         fi
@@ -97,7 +103,7 @@ if [ "$interactive_install" = "y" ]; then
     fi
     if [ -z "$agreement" ]; then
         while [[ $agreement != "y" && $agreement != "n" ]]; do
-            echo -e "Kuruluma başlıyorum? [y/n]: "
+            echo -e "Kuruluma başlansın mı ? [y/n]: "
             read -r agreement
         done
     fi
@@ -122,20 +128,20 @@ sleep 5
 export DEBIAN_FRONTEND=noninteractive
 
 if [ -z "$travis" ]; then
-    apt-get update -qq
+    apt-get update -qq >> /dev/null 2>&1
     apt-get --option=Dpkg::options::=--force-confmiss \
         --option=Dpkg::options::=--force-confold \
         --option=Dpkg::options::=--force-unsafe-io \
-        dist-upgrade --assume-yes --quiet
-    apt-get autoremove --purge -qq
-    apt-get autoclean -qq
+        dist-upgrade --assume-yes --quiet >> /dev/null 2>&1
+    apt-get autoremove --purge -qq >> /dev/null 2>&1
+    apt-get autoclean -qq >> /dev/null 2>&1
 fi
 
 apt-get \
     --option=Dpkg::options::=--force-confmiss \
     --option=Dpkg::options::=--force-confold \
     --assume-yes install haveged curl git unzip zip htop \
-    nload nmon ntp gnupg gnupg2 wget pigz tree tzdata ccze --quiet
+    nload nmon ntp gnupg gnupg2 wget pigz tree tzdata ccze --quiet >> /dev/null 2>&1
 
 if ! grep -q "time.cloudflare.com" /etc/systemd/timesyncd.conf; then
     sed -e 's/^#NTP=/NTP=time.cloudflare.com 0.ubuntu.pool.ntp.org 1.ubuntu.pool.ntp.org 2.ubuntu.pool.ntp.org 3.ubuntu.pool.ntp.org/' -i /etc/systemd/timesyncd.conf
@@ -144,10 +150,16 @@ fi
 
 export HISTSIZE=10000
 
+if [ ! -d "$HOME/ubuntu-nginx-web-server" ]; then
+    git clone https://github.com/VirtuBox/ubuntu-nginx-web-server.git "$HOME/ubuntu-nginx-web-server"
+else
+    git -C "$HOME/ubuntu-nginx-web-server" pull origin master
+fi
+
 if [ ! -f /etc/sysctl.d/60-plesk-tweaks.conf ]; then
     if [ "$plesk_srv_arch" = "x86_64" ]; then
         wget -qO /etc/sysctl.d/60-plesk-tweaks.conf \
-            https://raw.githubusercontent.com/fastdepo/fastpriviacy/master/sysctl.mustache 2> /dev/null
+            https://raw.githubusercontent.com/WordOps/WordOps/master/wo/cli/templates/sysctl.mustache
         if [ "$plesk_distro_version" = "bionic" ] || [ "$plesk_distro_version" = "disco" ] || [ "$plesk_distro_version" = "buster" ]; then
             modprobe tcp_bbr && echo 'tcp_bbr' >>/etc/modules-load.d/bbr.conf
             echo -e '\nnet.ipv4.tcp_congestion_control = bbr\nnet.ipv4.tcp_notsent_lowat = 16384' >>/etc/sysctl.d/60-plesk-tweaks.conf
@@ -161,9 +173,9 @@ fi
 
 if [ ! -x /opt/kernel-tweak.sh ]; then
     {
-        wget -qO /opt/kernel-tweak.sh https://raw.githubusercontent.com/fastdepo/fastpriviacy/master/kernel-tweak.sh 2> /dev/null
+        wget -qO /opt/kernel-tweak.sh https://raw.githubusercontent.com/VirtuBox/kernel-tweak/master/kernel-tweak.sh
         chmod +x /opt/kernel-tweak.sh
-        wget -qO /lib/systemd/system/kernel-tweak.service https://raw.githubusercontent.com/fastdepo/fastpriviacy/master/kernel-tweak.service 2> /dev/null
+        wget -qO /lib/systemd/system/kernel-tweak.service https://raw.githubusercontent.com/VirtuBox/kernel-tweak/master/kernel-tweak.service
         systemctl enable kernel-tweak.service
         systemctl start kernel-tweak.service
     } >>/tmp/plesk-install.log 2>&1
@@ -180,87 +192,91 @@ NET_INTERFACES_WAN=$(ip -4 route get 8.8.8.8 | grep -oP "dev [^[:space:]]+ " | c
     echo "net.ipv6.conf.$NET_INTERFACES_WAN.accept_ra_defrtr = 0"
 } >>/etc/sysctl.d/60-ubuntu-nginx-web-server.conf
 
+
 if [ "$mariadb_server_install" = "y" ]; then
-    echo ""
     {
-        wget -qO mariadb_repo_setup https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
-        chmod +x mariadb_repo_setup
-        ./mariadb_repo_setup --mariadb-server-version=$mariadb_version_install --skip-maxscale -y
-        rm mariadb_repo_setup
-        apt-get update -qq
+        wget -qO mariadb_repo_setup https://downloads.mariadb.com/MariaDB/mariadb_repo_setup >> /dev/null 2>&1
+        chmod +x mariadb_repo_setup >> /dev/null 2>&1
+        ./mariadb_repo_setup --mariadb-server-version=$mariadb_version_install --skip-maxscale -y >> /dev/null 2>&1
+        rm mariadb_repo_setup >> /dev/null 2>&1
+        apt-get update -qq >> /dev/null 2>&1
     } >>/tmp/plesk-install.log 2>&1
 fi
 
 if [ "$mariadb_server_install" = "y" ]; then
     if [ ! -d /etc/mysql ]; then
-        echo ""
-        MYSQL_ROOT_PASS=""
-        echo "mariadb-server-${mariadb_version_install} mysql-server/root_password password ${MYSQL_ROOT_PASS}" | debconf-set-selections
-        echo "mariadb-server-${mariadb_version_install} mysql-server/root_password_again password ${MYSQL_ROOT_PASS}" | debconf-set-selections
-        apt-get install -qq mariadb-server
+        MYSQL_ROOT_PASS="" >> /dev/null 2>&1
+        echo "mariadb-server-${mariadb_version_install} mysql-server/root_password password ${MYSQL_ROOT_PASS}" | debconf-set-selections >> /dev/null 2>&1
+        echo "mariadb-server-${mariadb_version_install} mysql-server/root_password_again password ${MYSQL_ROOT_PASS}" | debconf-set-selections >> /dev/null 2>&1
+        apt-get install -qq mariadb-server >> /dev/null 2>&1
         mysql -e "DROP USER ''@'localhost'" >/dev/null 2>&1
         mysql -e "DROP USER ''@'$(hostname)'" >/dev/null 2>&1
         mysql -e "DROP DATABASE test" >/dev/null 2>&1
-        mysql -e "FLUSH PRIVILEGES"
+        mysql -e "FLUSH PRIVILEGES" >> /dev/null 2>&1
     fi
 fi
 
 if [ "$mariadb_server_install" = "y" ]; then
-
-    cp /etc/mysql/my.cnf /etc/mysql/my.cnf.bak
-	wget https://raw.githubusercontent.com/fastdepo/fastpriviacy/master/my.cnf -O /etc/mysql/my.cnf 2> /dev/null
-    service mysql stop
-    mv /var/lib/mysql/ib_logfile0 /var/lib/mysql/ib_logfile0.bak
-    mv /var/lib/mysql/ib_logfile1 /var/lib/mysql/ib_logfile1.bak
-    echo -e '[Service]\nLimitNOFILE=500000' >/etc/systemd/system/mariadb.service.d/limits.conf
-    systemctl daemon-reload
-    service mysql start
+    cp /etc/mysql/my.cnf /etc/mysql/my.cnf.bak >> /dev/null 2>&1
+    cp -f "$HOME/ubuntu-nginx-web-server/etc/mysql/my.cnf" /etc/mysql/my.cnf >> /dev/null 2>&1
+    service mysql stop >> /dev/null 2>&1
+    mv /var/lib/mysql/ib_logfile0 /var/lib/mysql/ib_logfile0.bak >> /dev/null 2>&1
+    mv /var/lib/mysql/ib_logfile1 /var/lib/mysql/ib_logfile1.bak >> /dev/null 2>&1
+    echo -e '[Service]\nLimitNOFILE=500000' >/etc/systemd/system/mariadb.service.d/limits.conf >> /dev/null 2>&1
+    systemctl daemon-reload >> /dev/null 2>&1 
+    service mysql start >> /dev/null 2>&1
 
 fi
 
 if [ -z "$plesk_installed" ]; then
-    wget -O plesk-installer https://installer.plesk.com/plesk-installer
-    echo
+    wget -O plesk-installer https://installer.plesk.com/plesk-installer >> /dev/null 2>&1
+    echo >> /dev/null 2>&1
 
-    chmod +x ./plesk-installer
-    echo
+    chmod +x ./plesk-installer >> /dev/null 2>&1
+    echo >> /dev/null 2>&1
 
-    if ! { ./plesk-installer install release --components panel modsecurity fail2ban \
+    if ! { ./plesk-installer install release --components panel fail2ban modsecurity \
         l10n pmm mysqlgroup repair-kit \
-        spamassassin postfix dovecot \
+        roundcube spamassassin postfix dovecot \
         proftpd awstats mod_fcgid webservers \
-        nginx php7.4 php7.3 config-troubleshooter \
+        nginx php7.3 php7.4 config-troubleshooter \
         psa-firewall wp-toolkit letsencrypt \
-        imunifyav sslit; } >>/tmp/plesk-install.log 2>&1; then
+        imunifyav sslit; } >>/tmp/plesk-install.log 2>&1 >> /dev/null 2>&1; then
         echo
-        tail -f 50 /tmp/plesk-install.log | ccze -A
-        exit 1
+        echo "An error occurred! The installation of Plesk failed. Please see logged lines above for error handling!" >> /dev/null 2>&1
+        tail -f 50 /tmp/plesk-install.log | ccze -A >> /dev/null 2>&1
+        exit 1 
     fi
 
     if [ "$plesk_kvm_detec" = "kvm" ]; then
-        plesk bin vps_optimized --turn-on >>/tmp/plesk-install.log 2>&1
+        echo "Enable VPS Optimized Mode" >> /dev/null 2>&1
+        plesk bin vps_optimized --turn-on >>/tmp/plesk-install.log 2>&1 >> /dev/null 2>&1 
         echo
     fi
 
     export PSA_PASSWORD=$plesk_pass
 
     if [ -n "$activation_key" ]; then
-        /usr/sbin/plesk bin init_conf --init -email "$plesk_email" -passwd "" -name "$plesk_name" -license_agreed "$agreement"
-        /usr/sbin/plesk bin license --install "$activation_key"
+        echo "Starting initialization process of your Plesk server" >> /dev/null 2>&1
+        /usr/sbin/plesk bin init_conf --init -email "$plesk_email" -passwd "" -name "$plesk_name" -license_agreed "$agreement" >> /dev/null 2>&1
+        echo "Installing Plesk Activation Code" >> /dev/null 2>&1
+        /usr/sbin/plesk bin license --install "$activation_key" >> /dev/null 2>&1
         echo
     else
-        /usr/sbin/plesk bin init_conf --init -email "$plesk_email" -passwd "" -name "$plesk_name" -license_agreed "$agreement" -trial_license true
+        echo "Starting initialization process of your Plesk server" >> /dev/null 2>&1
+        /usr/sbin/plesk bin init_conf --init -email "$plesk_email" -passwd "" -name "$plesk_name" -license_agreed "$agreement" -trial_license true >> /dev/null 2>&1
     fi
 
     if [ "$plesk_ui" = "spv" ]; then
-        /usr/sbin/plesk bin poweruser --off
+        echo "Setting to Service Provider View" >> /dev/null 2>&1
+        /usr/sbin/plesk bin poweruser --off >> /dev/null 2>&1
         echo
     else
-        /usr/sbin/plesk bin poweruser --on
+        echo "Setting to Power user View" >> /dev/null 2>&1
+        /usr/sbin/plesk bin poweruser --on >> /dev/null 2>&1
         echo
     fi
 
-    echo "Setting Firewall to allow proper ports."
     {
         iptables -I INPUT -p tcp --dport 21 -j ACCEPT
         iptables -I INPUT -p tcp --dport 22 -j ACCEPT
@@ -274,10 +290,17 @@ if [ -z "$plesk_installed" ]; then
         iptables -I INPUT -p tcp --dport 8880 -j ACCEPT
     } >>/tmp/plesk-install.log 2>&1
 
-    plesk bin server_pref --update-web-app-firewall -waf-rule-engine on -waf-rule-set crs -waf-rule-set-update-period daily -waf-config-preset tradeoff
-	plesk bin locales --set-default tr-TR		
-	echo
+    echo
 fi
+# Enable Modsecurity
+# https://docs.plesk.com/en-US/onyx/administrator-guide/server-administration/web-application-firewall-modsecurity.73383/
+
+#echo "Turning on Modsecurity WAF Rules"
+#plesk bin server_pref --update-web-app-firewall -waf-rule-engine on -waf-rule-set tortix -waf-rule-set-update-period daily -waf-config-preset tradeoff
+#echo
+
+# Enable Fail2Ban and Jails
+# https://docs.plesk.com/en-US/onyx/cli-linux/using-command-line-utilities/ip_ban-ip-address-banning-fail2ban.73594/
 
 if [ "$fail2ban" = "yes" ]; then
     echo "Configuring Fail2Ban and its Jails"
@@ -287,7 +310,6 @@ if [ "$fail2ban" = "yes" ]; then
     /usr/sbin/plesk bin ip_ban --enable-jails plesk-proftpd
     /usr/sbin/plesk bin ip_ban --enable-jails plesk-postfix
     /usr/sbin/plesk bin ip_ban --enable-jails plesk-dovecot
-    /usr/sbin/plesk bin ip_ban --enable-jails plesk-modsecurity
     /usr/sbin/plesk bin ip_ban --enable-jails plesk-roundcube
     /usr/sbin/plesk bin ip_ban --enable-jails plesk-apache-badbot
     /usr/sbin/plesk bin ip_ban --enable-jails plesk-panel
@@ -297,50 +319,49 @@ if [ "$fail2ban" = "yes" ]; then
 fi
 
 if [ "$http2" = "yes" ]; then
-    echo "Activating http2"
-    /usr/sbin/plesk bin http2_pref --enable
+    echo "Activating http2" >> /dev/null 2>&1
+    /usr/sbin/plesk bin http2_pref --enable >> /dev/null 2>&1
     echo
 fi
 
-
-if [ "$pci_compliance" = "yes" ]; then
-    /usr/sbin/plesk sbin pci_compliance_resolver --enable all
+if [ "$pci_compliance" = "yes" ]; then >> /dev/null 2>&1
+    /usr/sbin/plesk sbin pci_compliance_resolver --enable all >> /dev/null 2>&1
 fi
 
-
-echo
-
-/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/2ae9cd0b-bc5c-4464-a12d-bd882c651392-xovi/download
-echo
-
-/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/b71916cf-614e-4b11-9644-a5fe82060aaf-revisium-antivirus/download
-echo ""
-
-/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/bebc4866-d171-45fb-91a6-4b139b8c9a1b-panel-migrator/download
-echo
-
-/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/e789f164-5896-4544-ab72-594632bcea01-rich-editor/download
-echo
-
-/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/b49f9b1b-e8cf-41e1-bd59-4509d92891f7-magicspam/download
-echo
-
-/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/05bdda39-792b-441c-9e93-76a6ab89c85a-panel-ini-editor/download
-echo
-
-/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/17ffcf2a-8e8f-4cb2-9265-1543ff530984-scheduled-backups-list/download
-echo
-
-wget https://raw.githubusercontent.com/VirtuBox/ubuntu-plesk-onyx/master/usr/local/psa/admin/conf/panel.ini -O /usr/local/psa/admin/conf/panel.ini
-echo
+echo "Installing Requested Plesk Extensions" >> /dev/null 2>&1
+echo >> /dev/null 2>&1
+echo "Installing SEO Toolkit" >> /dev/null 2>&1
+/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/2ae9cd0b-bc5c-4464-a12d-bd882c651392-xovi/download >> /dev/null 2>&1
+echo >> /dev/null 2>&1
+echo "Installing Revisium Antivirus for Websites" >> /dev/null 2>&1
+/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/b71916cf-614e-4b11-9644-a5fe82060aaf-revisium-antivirus/download >> /dev/null 2>&1
+echo "" >> /dev/null 2>&1
+echo "Installing Plesk Migration Manager" >> /dev/null 2>&1
+/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/bebc4866-d171-45fb-91a6-4b139b8c9a1b-panel-migrator/download >> /dev/null 2>&1
+echo >> /dev/null 2>&1
+echo "Installing Code Editor" >> /dev/null 2>&1
+/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/e789f164-5896-4544-ab72-594632bcea01-rich-editor/download >> /dev/null 2>&1
+echo >> /dev/null 2>&1
+echo "Installing MagicSpam" >> /dev/null 2>&1
+/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/b49f9b1b-e8cf-41e1-bd59-4509d92891f7-magicspam/download >> /dev/null 2>&1
+echo >> /dev/null 2>&1
+echo "Installing Panel.ini Extension" >> /dev/null 2>&1
+/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/05bdda39-792b-441c-9e93-76a6ab89c85a-panel-ini-editor/download >> /dev/null 2>&1
+echo >> /dev/null 2>&1
+echo "Installing Schedule Backup list Extension" >> /dev/null 2>&1
+/usr/sbin/plesk bin extension --install-url https://ext.plesk.com/packages/17ffcf2a-8e8f-4cb2-9265-1543ff530984-scheduled-backups-list/download >> /dev/null 2>&1
+echo >> /dev/null 2>&1
+echo "Set custom panel.ini config" >> /dev/null 2>&1
+wget https://raw.githubusercontent.com/VirtuBox/ubuntu-plesk-onyx/master/usr/local/psa/admin/conf/panel.ini -O /usr/local/psa/admin/conf/panel.ini >> /dev/null 2>&1
+echo >> /dev/null 2>&1
 
 if [ "$clone" = "on" ]; then
-    /usr/sbin/plesk bin cloning --update -prepare-public-image true -reset-license true -skip-update true
+    /usr/sbin/plesk bin cloning --update -prepare-public-image true -reset-license true -skip-update true >> /dev/null 2>&1
 else
-    echo "Giriş Bilgileriniz."
-    /usr/sbin/plesk login
+    echo "Here is your login" >> /dev/null 2>&1
+    /usr/sbin/plesk login 
 fi
 
 echo
-echo "Plesk Kuruldu."
+echo "Plesk Panel Kuruldu"
 echo
